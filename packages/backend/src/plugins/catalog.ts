5fce230a2c8477e35ec7e4aa464d030350a5e686
Andre Muijen <andre.muijen@rabobank.nl>
2024-01-26 16:35:39 +0100

    Add more AzDo configuration, but it's not refreshing

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -3,6 +3,7 @@ import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-m
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
 import { AzureDevOpsAnnotatorProcessor } from '@backstage/plugin-azure-devops-backend';
+import { AzureDevOpsEntityProvider } from '@backstage/plugin-catalog-backend-module-azure';
 
 export default async function createPlugin(
   env: PluginEnvironment,
@@ -10,6 +11,20 @@ export default async function createPlugin(
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
   builder.addProcessor(AzureDevOpsAnnotatorProcessor.fromConfig(env.config));
+
+  builder.addEntityProvider(
+    AzureDevOpsEntityProvider.fromConfig(env.config, {
+      logger: env.logger,
+      // optional: alternatively, use scheduler with schedule defined in app-config.yaml
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 30 },
+        timeout: { minutes: 3 },
+      }),
+      // optional: alternatively, use schedule
+      scheduler: env.scheduler,
+    }),
+  );
+
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
